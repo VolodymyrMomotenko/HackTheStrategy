21902c66217e71f68316c2c58a887df33199597b
“Maksym <2950374p@student.gla.ac.uk>
2025-10-25 13:41:36 +0100

    Map window

---
+++ GameMao.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+
+public class GameMao{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Game");
+        frame.setSize(1000, 600); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null); 
+        frame.setVisible(true);
+    }
+    
+}
